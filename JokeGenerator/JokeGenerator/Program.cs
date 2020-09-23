c450df85c23bb83d5fe2f11c352f939894997fa3
kavyajalla <kavyajalla139@gmail.com>
2020-09-23 10:27:05 -0400

    Add files via upload

---
+++ JokeGenerator/JokeGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using JokeGenerator.ClientAPI;
+using JokeGenerator.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JokeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Title for Console Application
+            Console.Title = "Joke Generator";
+            
+            ServiceCollection serviceCollection = new ServiceCollection();
+            AddServices(serviceCollection);
+
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            serviceProvider.GetService<Generator>().GenerateJokes();
+        }
+
+        static void AddServices(IServiceCollection services)
+        {
+            services.AddHttpClient<IChuckNorrisAPI, ChuckNorrisAPI>();
+            services.AddHttpClient<INameGeneratorAPI, NameGeneratorAPI>();
+            services.AddSingleton<IJokeService, JokeService>(x => new JokeService(x.GetService<IChuckNorrisAPI>(), x.GetService<INameGeneratorAPI>()));
+            services.AddSingleton(x => new Generator(x.GetService<IJokeService>()));
+        }
+    }
+}
